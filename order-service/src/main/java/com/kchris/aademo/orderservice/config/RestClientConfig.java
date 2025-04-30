83b3ede883ac68a2975fca49ec4447e08c079090
Kostas Christodoulou <christodoulou.galanopoulos@gmail.com>
2025-04-30 19:22:15 +0300

    Dockerised applications

--- order-service/src/main/java/com/kchris/aademo/orderservice/config/RestClientConfig.java
+++ order-service/src/main/java/com/kchris/aademo/orderservice/config/RestClientConfig.java
@@ -1,5 +1,6 @@
 package com.kchris.aademo.orderservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,17 +8,23 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+  @Value("${external.user-service.url}")
+  private String userServiceUrl;
+
+  @Value("${external.product-service.url}")
+  private String productServiceUrl;
+
   @Bean("userServiceRestClient")
   public RestClient userServiceRestClient(RestClient.Builder builder) {
     return builder
-        .baseUrl("http://localhost:8081") // TODO -> Adjust to your user-service port
+        .baseUrl(userServiceUrl)
         .build();
   }
 
   @Bean("productServiceRestClient")
   public RestClient productServiceRestClient(RestClient.Builder builder) {
     return builder
-        .baseUrl("http://localhost:8082") // TODO -> Adjust to your product-service port
+        .baseUrl(productServiceUrl)
         .build();
   }
 }
