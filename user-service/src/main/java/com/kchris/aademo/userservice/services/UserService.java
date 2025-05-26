3341983726df37f199e368d17c1c80a70a36a3a2
XristodoulouKostas <45286216+XristodoulouKostas@users.noreply.github.com>
2025-05-26 19:47:26 +0300

    Add logs (#4)

--- user-service/src/main/java/com/kchris/aademo/userservice/services/UserService.java
+++ user-service/src/main/java/com/kchris/aademo/userservice/services/UserService.java
@@ -5,8 +5,10 @@ import com.kchris.aademo.userservice.repositories.UserEntity;
 import com.kchris.aademo.userservice.repositories.UserRepository;
 import java.util.Optional;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserService {
 
