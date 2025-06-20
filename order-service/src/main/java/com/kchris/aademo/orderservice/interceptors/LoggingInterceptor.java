85cbc60ae8c09806834e2865e00b2cb9bb34e255
kchristodoulou <christodoulou.galanopoulos@gmail.com>
2025-06-20 18:08:25 +0300

    Return the correlation-id as a header in the response

---
+++ order-service/src/main/java/com/kchris/aademo/orderservice/interceptors/LoggingInterceptor.java
@@ -0,0 +1,21 @@
+package com.kchris.aademo.orderservice.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class LoggingInterceptor implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    String correlationId = UUID.randomUUID().toString();
+    MDC.put("correlation-id", correlationId);
+    response.addHeader("Correlation-Id", correlationId);
+    return true;
+  }
+
+}
