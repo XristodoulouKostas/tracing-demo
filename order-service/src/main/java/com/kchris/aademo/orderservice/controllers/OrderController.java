8f1ec64c1bd31ec3976e19bc24f96d50aaba78f7
Kostas Christodoulou Galanopoulos <christodoulou.galanopoulos@gmail.com>
2025-11-29 19:40:24 +0200

    Add userId in the baggage

--- order-service/src/main/java/com/kchris/aademo/orderservice/controllers/OrderController.java
+++ order-service/src/main/java/com/kchris/aademo/orderservice/controllers/OrderController.java
@@ -2,43 +2,38 @@ package com.kchris.aademo.orderservice.controllers;
 
 import com.kchris.aademo.orderservice.domain.Order;
 import com.kchris.aademo.orderservice.services.OrderService;
-import io.micrometer.tracing.BaggageInScope;
-import io.micrometer.tracing.Tracer;
-import java.util.UUID;
+import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
 @RequestMapping("orders")
 public class OrderController {
 
-  private final Tracer tracer;
-  private final OrderService orderService;
+    private final OrderService orderService;
 
-  public OrderController(Tracer tracer, OrderService orderService) {
-    this.tracer = tracer;
-    this.orderService = orderService;
-  }
-
-  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  @ResponseStatus(HttpStatus.CREATED)
-  public Order createOrder(@RequestBody CreateOrderRequest createOrderRequest) {
-    addUserIdBaggage(createOrderRequest.userId());
-    log.info("Creating order {}", createOrderRequest);
-    return orderService.createOrder(createOrderRequest);
-  }
-
-  // TODO Test
-  private void addUserIdBaggage(UUID userId) {
-    try (BaggageInScope baggage = tracer.createBaggageInScope("user-id", userId.toString())) {
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
     }
-  }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Order createOrder(@RequestBody CreateOrderRequest createOrderRequest) {
+        try (Scope scope = addUserIdBaggage(createOrderRequest.userId())) {
+            log.info("Creating order {}", createOrderRequest);
+            return orderService.createOrder(createOrderRequest);
+        }
+    }
+
+    private Scope addUserIdBaggage(UUID userId) {
+        Baggage baggage = Baggage.current().toBuilder().put("userId", userId.toString()).build();
+        return baggage.storeInContext(Context.current()).makeCurrent();
+    }
 }
