72c7839ae24820e31fbc8c9257fb03357affa427
Kostas Christodoulou Galanopoulos <christodoulou.galanopoulos@gmail.com>
2025-11-30 15:43:02 +0200

    Log Kafka Headers on downstream services

--- notification-service/src/main/java/com/kchris/aademo/notificationservice/listeners/OrderCreatedListener.java
+++ notification-service/src/main/java/com/kchris/aademo/notificationservice/listeners/OrderCreatedListener.java
@@ -5,18 +5,20 @@ import commonutilities.ArtificialError;
 import java.time.temporal.ChronoUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class OrderCreatedListener {
 
-  private ArtificialError artificialError = new ArtificialError();
+  private final ArtificialError artificialError = new ArtificialError();
 
   @KafkaListener(topics = "order-created", groupId = "notification-service-group", containerFactory = "kafkaListenerContainerFactory")
-  public void handleOrderCreated(OrderCreatedEvent message) {
+  public void handleOrderCreated(Message<OrderCreatedEvent> message) {
+    log.info("Message headers: {}", message.getHeaders());
     artificialError.randomlyFailWith(0.1, new RuntimeException("Could not send the notification"));
     artificialError.randomlyAddDelay(0.3, 10, ChronoUnit.SECONDS);
-    log.info("Notification sent for order: " + message);
+    log.info("Notification sent for order: " + message.getPayload());
   }
 }
