54a7dda3091c0896a1ebf9c4e60e3d7cadca7224
Kostas Christodoulou Galanopoulos <christodoulou.galanopoulos@gmail.com>
2025-11-30 19:07:34 +0200

    Minor refactoring

--- notification-service/src/main/java/com/kchris/aademo/notificationservice/listeners/OrderCreatedListener.java
+++ notification-service/src/main/java/com/kchris/aademo/notificationservice/listeners/OrderCreatedListener.java
@@ -2,22 +2,21 @@ package com.kchris.aademo.notificationservice.listeners;
 
 import com.kchris.aademo.notificationservice.domain.events.OrderCreatedEvent;
 import commonutilities.ArtificialError;
+import java.time.temporal.ChronoUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import java.time.temporal.ChronoUnit;
 
 @Slf4j
 @Service
 public class OrderCreatedListener {
 
-    private ArtificialError artificialError = new ArtificialError();
+  private final ArtificialError artificialError = new ArtificialError();
 
-    @KafkaListener(topics = "order-created", groupId = "notification-service-group", containerFactory = "kafkaListenerContainerFactory")
-    public void handleOrderCreated(OrderCreatedEvent message) {
-        artificialError.randomlyFailWith(0.1, new RuntimeException("Could not send the notification"));
-        artificialError.randomlyAddDelay(0.3, 10, ChronoUnit.SECONDS);
-        log.info("Notification sent for order: " + message);
-    }
+  @KafkaListener(topics = "order-created", groupId = "notification-service-group", containerFactory = "kafkaListenerContainerFactory")
+  public void handleOrderCreated(OrderCreatedEvent message) {
+    artificialError.randomlyFailWith(0.1, new RuntimeException("Could not send the notification"));
+    artificialError.randomlyAddDelay(0.3, 10, ChronoUnit.SECONDS);
+    log.info("Notification sent for order: " + message);
+  }
 }
