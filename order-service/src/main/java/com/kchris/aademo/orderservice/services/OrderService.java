1ad95e1ae6112e9c01caaa4846512ada8798c601
Kostas Christodoulou Galanopoulos <christodoulou.galanopoulos@gmail.com>
2025-12-04 21:26:52 +0200

    Reduced the number of errors

--- order-service/src/main/java/com/kchris/aademo/orderservice/services/OrderService.java
+++ order-service/src/main/java/com/kchris/aademo/orderservice/services/OrderService.java
@@ -7,9 +7,7 @@ import com.kchris.aademo.orderservice.domain.Order;
 import com.kchris.aademo.orderservice.domain.events.OrderCreatedEvent;
 import com.kchris.aademo.orderservice.enums.OrderStatus;
 import com.kchris.aademo.orderservice.repositories.OrderRepository;
-import commonutilities.ArtificialError;
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,13 +24,10 @@ public class OrderService {
   private final UserService userService;
   private final KafkaTemplate<String, Object> kafkaTemplate;
 
-  private final ArtificialError artificialError = new ArtificialError();
-
   public Order createOrder(CreateOrderRequest createOrderRequest) {
-    artificialError.randomlyFailWith(0.1, new RuntimeException("Could not send the notification"));
-    artificialError.randomlyAddDelay(0.05, 10, ChronoUnit.SECONDS);
     userService.verifyThatUserIsAllowedToOrder(createOrderRequest.userId());
-    productService.verifyThereAreEnoughItems(createOrderRequest.productId(), createOrderRequest.quantity());
+    productService.verifyThereAreEnoughItems(createOrderRequest.productId(),
+        createOrderRequest.quantity());
 
     Order order = Order.builder()
         .id(UUID.randomUUID())
@@ -44,12 +39,8 @@ public class OrderService {
         .updatedAt(Instant.now())
         .build();
 
-    artificialError.randomlyFailWith(0.1, new RuntimeException("Could not send the notification"));
-    artificialError.randomlyAddDelay(0.05, 10, ChronoUnit.SECONDS);
     orderRepository.insert(order);
     log.info("Order {} created successfully", order.id());
-    artificialError.randomlyFailWith(0.1, new RuntimeException("Could not send the notification"));
-    artificialError.randomlyAddDelay(0.05, 10, ChronoUnit.SECONDS);
     kafkaTemplate.send("order-created", OrderCreatedEvent.forOrder(order));
     return order;
   }
