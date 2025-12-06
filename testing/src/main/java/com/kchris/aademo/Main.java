9ff3b6b7f2687c86625122ca925382f048439eba
Kostas Christodoulou Galanopoulos <christodoulou.galanopoulos@gmail.com>
2025-12-06 13:08:23 +0200

    Modified the test scenarios

--- testing/src/main/java/com/kchris/aademo/Main.java
+++ testing/src/main/java/com/kchris/aademo/Main.java
@@ -2,7 +2,6 @@ package com.kchris.aademo;
 
 import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class Main {
@@ -10,13 +9,8 @@ public class Main {
   public static void main(String[] args) {
     OrderService orderService = new OrderService();
 
-    List<CompletableFuture<HttpResponse<String>>> futures = getUsers().stream()
-        .map(user -> orderService.createOrder(
-            new CreateOrderRequest(
-                UUID.fromString(user),
-                UUID.fromString("f1e2d3c4-b5a6-11ec-9fea-0242ac120001"),
-                1
-            )))
+    List<CompletableFuture<HttpResponse<String>>> futures = getRequests().stream()
+        .map(orderService::createOrder)
         .toList();
 
     CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
@@ -36,29 +30,48 @@ public class Main {
     }
   }
 
-  private static List<String> getUsers() {
+  private static List<CreateOrderRequest> getRequests() {
     return List.of(
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120001",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120002",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120003",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120004",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120005",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120113",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120005",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120006",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac127776",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120007",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120008",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac120009",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac12000a",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac12000c",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac12000c",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac12000d",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac12000e",
-        "a1b2c3d4-e5f6-11ec-8fea-0242ac12000f"
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac120001",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac120001", 100),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac120001",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac120001", 20),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac120002", // Fail - user
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac120001", 20),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac120003",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac120001", 20),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac120003",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac120004", 85), // Fail - product quantity
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac120003",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac120003",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac120004", // Fail - user
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b", // Fail - Unknown product
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12111b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12011b", // Fail - user
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1),
+        new CreateOrderRequest("a1b2c3d4-e5f6-11ec-8fea-0242ac12000b",
+            "f1e2d3c4-b5a6-11ec-9fea-0242ac12000b", 1)
     );
   }
-
 }
