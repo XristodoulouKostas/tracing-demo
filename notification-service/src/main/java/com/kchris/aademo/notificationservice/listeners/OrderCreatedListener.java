6bca4f49b73e0ebc9119fa8eb95f0383599ac73a
Kostas Christodoulou Galanopoulos <christodoulou.galanopoulos@gmail.com>
2025-12-06 13:51:31 +0200

    Added random errors
    # Conflicts:
    #	notification-service/src/main/java/com/kchris/aademo/notificationservice/listeners/OrderCreatedListener.java

--- notification-service/src/main/java/com/kchris/aademo/notificationservice/listeners/OrderCreatedListener.java
+++ notification-service/src/main/java/com/kchris/aademo/notificationservice/listeners/OrderCreatedListener.java
@@ -5,6 +5,7 @@ import commonutilities.ArtificialError;
 import java.time.temporal.ChronoUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -14,9 +15,9 @@ public class OrderCreatedListener {
   private final ArtificialError artificialError = new ArtificialError();
 
   @KafkaListener(topics = "order-created", groupId = "notification-service-group", containerFactory = "kafkaListenerContainerFactory")
-  public void handleOrderCreated(OrderCreatedEvent message) {
-    artificialError.randomlyFailWith(0.1, new RuntimeException("Could not send the notification"));
+  public void handleOrderCreated(Message<OrderCreatedEvent> message) {
+    log.info("Message headers: {}", message.getHeaders());
     artificialError.randomlyAddDelay(0.3, 10, ChronoUnit.SECONDS);
-    log.info("Notification sent for order: " + message);
+    log.info("Notification sent for order: " + message.getPayload());
   }
 }
--- user-service/src/main/java/com/kchris/aademo/userservice/services/UserService.java
+++ user-service/src/main/java/com/kchris/aademo/userservice/services/UserService.java
@@ -3,6 +3,8 @@ package com.kchris.aademo.userservice.services;
 import com.kchris.aademo.userservice.domain.User;
 import com.kchris.aademo.userservice.repositories.UserEntity;
 import com.kchris.aademo.userservice.repositories.UserRepository;
+import commonutilities.ArtificialError;
+import java.time.temporal.ChronoUnit;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
@@ -14,11 +16,15 @@ public class UserService {
 
   private final UserRepository userRepository;
 
+  private final ArtificialError artificialError = new ArtificialError();
+
   public UserService(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
   public Optional<User> getUserById(UUID id) {
+    artificialError.randomlyAddDelay(0.05, 5, ChronoUnit.SECONDS);
+    artificialError.randomlyFailWith(0.2, "Could not retrieve users from DB");
     return userRepository.findById(id).map(UserEntity::toDomain);
   }
 
